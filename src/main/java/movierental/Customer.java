0517ca78c39a8cc6d14e48a4e392c05c62de06ce
Pierre CAMPISTRON <pier.campistron@gmail.com>
2024-02-12 15:54:15 +0100

    Inline de variable

--- src/main/java/movierental/Customer.java
+++ src/main/java/movierental/Customer.java
@@ -26,10 +26,9 @@ public class Customer {
         String result = addHeaderLine();
 
         for (Rental rental : rentals) {
-            double thisAmount = rental.determineAmounts();
             frequentRenterPoints += rental.frequentRenterPoints();
-            result += rental.showFiguresForThisRental(thisAmount);
-            totalAmount += thisAmount;
+            result += rental.showFiguresForThisRental(rental.determineAmounts());
+            totalAmount += rental.determineAmounts();
         }
 
         // add footer lines
